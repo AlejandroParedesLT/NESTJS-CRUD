65f4851efbfcc6d59f854a3d8a23180278a25733
Alejandro Paredes <alejocangrejo10@gmail.com>
2023-08-13 11:41:43 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
+@Module({
+  imports:
+    [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'test',
+        entities: [User],
+        synchronize: true,
+      }),
+    UsersModule],
+  
+})
+export class AppModule {}
